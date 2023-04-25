a533f508433e44c3c2a750ed397bac9aff95d911
shifanrasik <87424484+shifanrasik@users.noreply.github.com>
2023-04-25 09:22:22 +0530

    Add files via upload

---
+++ AdsenseSolution/Digital solution/src/php/formsdigital.php
@@ -0,0 +1,15 @@
+<html>
+    <body>
+        <?php
+        $name = $_POST["name"];
+        $phone = $_POST["phone"];
+        $emailid = $_POST["emailid"];
+        $bname = $_POST["bname"];
+        $ctype = $_POST["ctype"];
+        ?>
+        Hi <?php echo $name;?><br>
+        we got your inforamation.<br>
+        we will respond to you as soon as possible
+
+    </body>
+</html>
